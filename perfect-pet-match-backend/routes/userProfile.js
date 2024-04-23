24d74363efede80fa12144153f2f40f0275844a0
jtorres1195 <jtorres1195@yahoo.com>
2024-04-22 20:02:04 -0700

    Updating bcrypt

--- perfect-pet-match-backend/routes/userProfile.js
+++ perfect-pet-match-backend/routes/userProfile.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 const SavedPets = require('../models/SavedPets');
 const { verifyToken } = require('../middleware/auth');
