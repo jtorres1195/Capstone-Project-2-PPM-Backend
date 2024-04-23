b3ad7306cd9ba08ad97c541722f01450b19a50a7
jtorres1195 <jtorres1195@yahoo.com>
2024-04-22 22:59:40 -0700

    idk

--- perfect-pet-match-backend/app.js
+++ perfect-pet-match-backend/app.js
@@ -37,7 +37,7 @@ app.use((err, req, res, next) => {
     res.status(500).send('Internal Server Error.');
 });
 
-const PORT = process.env.PORT || 3001;  
+const PORT = process.env.PORT || 10000;  
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
